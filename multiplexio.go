9e3ff3ea7744bef27f01443ec8176c633ff4452b
Brice Jaglin <bjaglin@gmail.com>
2014-11-14 08:11:26 +0100

    initial implementation & tests

---
+++ multiplexio.go
@@ -0,0 +1,141 @@
+package multiplexio
+
+import (
+	"bufio"
+	"io"
+	"sort"
+	"time"
+)
+
+// Reader aggregating, according to a given ordering, tokens extracted
+// concurrently from a set of io.Reader
+type Reader struct {
+	pipeReader *io.PipeReader
+}
+
+type extractedToken struct {
+	bytes         []byte
+	scanSemaphore chan struct{}
+}
+
+func sortByTokenDescString(extractedTokens []extractedToken) sort.Interface {
+	return byTokenDescString(extractedTokens)
+}
+
+type byTokenDescString []extractedToken
+
+func (b byTokenDescString) Len() int {
+	return len(b)
+}
+func (b byTokenDescString) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+func (b byTokenDescString) Less(i, j int) bool {
+	return string(b[i].bytes) > string(b[j].bytes)
+}
+
+// NewReader returns a new io.ReadCloser multiplexing a set of io.Reader
+func NewReader(readers ...io.Reader) *Reader {
+	// configuration
+	var (
+		firstTimeout = time.Second           // how long do we wait for an initial token from each reader?
+		timeout      = time.Millisecond      // how long do we wait for tokens after at least one reader produced one?
+		split        = bufio.ScanLines       // tokenizing function
+		sortBy       = sortByTokenDescString // sorting function defining which token gets out first
+	)
+
+	// plumbing tools
+	var (
+		pipeReader, pipeWriter = io.Pipe()
+		ch                     = make(chan extractedToken)
+	)
+
+	// goroutines scanning & extracting tokens to feed them into
+	// the main goroutine via the channel
+	reader2chan := func(reader io.Reader) {
+		var (
+			scanSemaphore = make(chan struct{})
+			scanner       = bufio.NewScanner(reader)
+		)
+		scanner.Split(split)
+		for scanner.Scan() {
+			bytes := append(scanner.Bytes(), byte('\n'))
+			// send the extracted bytes along with a semaphore to
+			// let the main goroutine throttle the scanning
+			ch <- extractedToken{
+				bytes:         bytes,
+				scanSemaphore: scanSemaphore,
+			}
+			// block until we are asked to consume more
+			<-scanSemaphore
+		}
+		// signal that there is nothing else coming from that routine
+		ch <- extractedToken{}
+		// TODO: better error handling: check scanner.Err()
+	}
+	for _, reader := range readers {
+		go reader2chan(reader)
+	}
+
+	// goroutine feeding into PipeWriter as tokens arrive
+	go func() {
+		var (
+			scanning        = len(readers)
+			extractedTokens = make([]extractedToken, 0, len(readers))
+			blockMax        = firstTimeout
+		)
+		for scanning != 0 {
+			var (
+				timeoutOccured = false
+				tokenTimer     = time.After(blockMax)
+			)
+			for scanning != 0 && !timeoutOccured {
+				timer := tokenTimer
+				if len(extractedTokens) == 0 {
+					// nothing is extracted yet so we need that token to do
+					// anything, so no need for timeout that would result in
+					// busy-polling
+					timer = nil
+				}
+				select {
+				case extractedToken, ok := <-ch:
+					if ok && extractedToken.scanSemaphore != nil {
+						extractedTokens = append(extractedTokens, extractedToken)
+					}
+					scanning--
+				case <-timer:
+					timeoutOccured = true
+				}
+			}
+			if len(extractedTokens) > 0 {
+				// sort to get the token we want at the tail
+				sort.Sort(sortBy(extractedTokens))
+				// get the tail of the list
+				extractedToken := extractedTokens[len(extractedTokens)-1]
+				// strip that tail for the next run
+				extractedTokens = extractedTokens[0 : len(extractedTokens)-1]
+				// dump the bytes, blocking until they are consumed
+				if _, err := pipeWriter.Write(extractedToken.bytes); err != nil {
+					// TODO: gracefully cleanup reader2chan goroutines? close semaphores?
+					close(ch)
+					break
+				}
+				// signal that we want more data from the scanner we got that token from
+				extractedToken.scanSemaphore <- struct{}{}
+				scanning++
+			}
+			blockMax = timeout
+		}
+		pipeWriter.Close()
+	}()
+
+	return &Reader{pipeReader}
+}
+
+func (mr *Reader) Read(p []byte) (n int, err error) {
+	return mr.pipeReader.Read(p)
+}
+
+func (mr *Reader) Close() error {
+	return mr.pipeReader.Close()
+}
---
+++ multiplexio_test.go
@@ -0,0 +1,256 @@
+package multiplexio
+
+import (
+	"io"
+	"io/ioutil"
+	"reflect"
+	"testing"
+	"time"
+)
+
+const (
+	line1          = "1 foo\n"
+	line2          = "2 barbar\n"
+	line3          = "3 quxquxqux\n"
+	line4          = "4 bazbazbazbaz\n"
+	unfinishedLine = "5 thisisnotacompletetoken"
+)
+
+func concatenatedStringsAsBytes(strings ...string) []byte {
+	buf := make([]byte, 0, 1024)
+	for _, s := range strings {
+		buf = append(buf, []byte(s)...)
+	}
+
+	return buf
+}
+
+func readOneByteAtTheTime(src io.Reader, written *int) []byte {
+	buf := make([]byte, 1024)
+	for {
+		n, err := io.ReadAtLeast(src, buf[*written:], 1)
+		if err != nil {
+			break
+		}
+		*written = *written + n
+	}
+	return buf[:*written]
+}
+
+func TestReaderInterfaces(t *testing.T) {
+	var _ io.ReadCloser = NewReader()
+}
+
+func TestLazyWrappedReaderFetching(t *testing.T) {
+	var (
+		pipeReader, pipeWriter = io.Pipe()
+		reader                 = NewReader(pipeReader)
+	)
+	go func() {
+		io.WriteString(pipeWriter, line1)
+		io.WriteString(pipeWriter, line1)
+		io.WriteString(pipeWriter, line1)
+		pipeWriter.Close()
+	}()
+	// ask for enough bytes to get the first token
+	io.CopyN(ioutil.Discard, reader, int64(len(line1)))
+	go func() {
+		// asking for one byte should consume only one more token
+		io.CopyN(ioutil.Discard, reader, 1)
+	}()
+	time.Sleep(20 * time.Millisecond)
+	unconsumed, _ := io.Copy(ioutil.Discard, pipeReader)
+	// leaving one token that we had no reason to fetch
+	expected := int64(len(line1))
+	if unconsumed != expected {
+		t.Errorf("%v bytes unconsumed by the implementation, %v expected", unconsumed, expected)
+	}
+}
+
+func TestForwardingSingleSlowReader(t *testing.T) {
+	var (
+		pipeReader, pipeWriter = io.Pipe()
+		reader                 = NewReader(pipeReader)
+	)
+	go func() {
+		time.Sleep(20 * time.Millisecond)
+		io.WriteString(pipeWriter, line1)
+		time.Sleep(time.Second)
+		io.WriteString(pipeWriter, line2)
+		time.Sleep(20 * time.Millisecond)
+		pipeWriter.Close()
+	}()
+	written, _ := io.Copy(ioutil.Discard, reader)
+	expected := int64(len(line1 + line2))
+	if written != expected {
+		t.Errorf("%v bytes copied, %v expected", written, expected)
+	}
+}
+
+func TestForwardingOneSlowerReader(t *testing.T) {
+	var (
+		pipeReader1, pipeWriter1 = io.Pipe()
+		pipeReader2, pipeWriter2 = io.Pipe()
+		reader                   = NewReader(pipeReader1, pipeReader2)
+	)
+	go func() {
+		io.WriteString(pipeWriter1, line1)
+		pipeWriter1.Close()
+	}()
+	go func() {
+		io.WriteString(pipeWriter2, line2)
+		time.Sleep(20 * time.Millisecond)
+		io.WriteString(pipeWriter2, line3)
+		time.Sleep(20 * time.Millisecond)
+		io.WriteString(pipeWriter2, line4)
+		pipeWriter2.Close()
+	}()
+	written, _ := io.Copy(ioutil.Discard, reader)
+	expected := int64(len(line1 + line2 + line3 + line4))
+	if written != expected {
+		t.Errorf("%v bytes copied, %v expected", written, expected)
+	}
+}
+
+func TestForwardingSingleHangingReader(t *testing.T) {
+	var (
+		pipeReader, pipeWriter = io.Pipe()
+		reader                 = NewReader(pipeReader)
+		written                int
+		doneReading            bool
+	)
+	go func() {
+		io.WriteString(pipeWriter, line1)
+		io.WriteString(pipeWriter, unfinishedLine)
+	}()
+	go func() {
+		readOneByteAtTheTime(reader, &written)
+		doneReading = true
+	}()
+	time.Sleep(20 * time.Millisecond)
+	expected := len(line1)
+	if written != expected {
+		t.Errorf("%v bytes copied, %v expected", written, expected)
+	}
+	if doneReading {
+		t.Errorf("reader expected to block but was done reading")
+	}
+}
+
+func TestForwardingOneHangingReader(t *testing.T) {
+	var (
+		pipeReader1, pipeWriter1 = io.Pipe()
+		pipeReader2, pipeWriter2 = io.Pipe()
+		pipeReader3, _           = io.Pipe()
+		reader                   = NewReader(pipeReader1, pipeReader2, pipeReader3)
+		written                  int
+		doneReading              bool
+	)
+	go func() {
+		io.WriteString(pipeWriter1, line1)
+		pipeWriter1.Close()
+	}()
+	go func() {
+		io.WriteString(pipeWriter2, line2)
+		pipeWriter2.Close()
+	}()
+	go func() {
+		readOneByteAtTheTime(reader, &written)
+		doneReading = true
+	}()
+	time.Sleep(time.Second + 20*time.Millisecond)
+	expected := len(line1 + line2)
+	if written != expected {
+		t.Errorf("%v bytes copied, %v expected", written, expected)
+	}
+	if doneReading {
+		t.Errorf("reader expected to block but was done reading")
+	}
+}
+
+func TestForwardingUnfinishedTrailingToken(t *testing.T) {
+	var (
+		pipeReader, pipeWriter = io.Pipe()
+		reader                 = NewReader(pipeReader)
+	)
+	go func() {
+		io.WriteString(pipeWriter, line1)
+		io.WriteString(pipeWriter, line2)
+		io.WriteString(pipeWriter, unfinishedLine)
+		pipeWriter.Close()
+	}()
+	written, _ := io.Copy(ioutil.Discard, reader)
+	expectedAtLeast := int64(len(line1 + line2))
+	if written < expectedAtLeast {
+		t.Errorf("%v bytes copied, at least %v expected", written, expectedAtLeast)
+	}
+}
+
+func TestOrderingSequential(t *testing.T) {
+	var (
+		pipeReader1, pipeWriter1 = io.Pipe()
+		pipeReader2, pipeWriter2 = io.Pipe()
+		reader                   = NewReader(pipeReader1, pipeReader2)
+		expected                 = make([]byte, 0, 1024)
+	)
+	go func() {
+		// exercise the initial waiting by delaying token
+		// availability in the stream that should come first
+		time.Sleep(20 * time.Millisecond)
+		for i := 0; i < 10; i++ {
+			io.WriteString(pipeWriter1, line1)
+		}
+		pipeWriter1.Close()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			io.WriteString(pipeWriter2, line2)
+		}
+		pipeWriter2.Close()
+	}()
+	actual := readOneByteAtTheTime(reader, new(int))
+	for i := 0; i < 10; i++ {
+		expected = append(expected, []byte(line1)...)
+	}
+	for i := 0; i < 100; i++ {
+		expected = append(expected, []byte(line2)...)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("`%v` read, `%v` expected", string(actual), string(expected))
+	}
+}
+
+func TestOrderingInterlaced(t *testing.T) {
+	var (
+		pipeReader1, pipeWriter1 = io.Pipe()
+		pipeReader2, pipeWriter2 = io.Pipe()
+		reader                   = NewReader(pipeReader1, pipeReader2)
+	)
+	go func() {
+		// exercise the initial waiting by delaying token
+		// availability in the stream that should come first
+		time.Sleep(20 * time.Millisecond)
+		io.WriteString(pipeWriter1, line1)
+		io.WriteString(pipeWriter1, line1)
+		io.WriteString(pipeWriter1, line3)
+		pipeWriter1.Close()
+	}()
+	go func() {
+		io.WriteString(pipeWriter2, line2)
+		io.WriteString(pipeWriter2, line4)
+		io.WriteString(pipeWriter2, line4)
+		pipeWriter2.Close()
+	}()
+	actual := readOneByteAtTheTime(reader, new(int))
+	expected := concatenatedStringsAsBytes(
+		line1,
+		line1,
+		line2,
+		line3,
+		line4,
+		line4,
+	)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("`%v` read, `%v` expected", string(actual), string(expected))
+	}
+}
